1a13284aa82e8f8d012aa50612bc538c776cc6b8
AliErcanOzgokce <aliercanozgokce@gmail.com>
2022-05-02 13:10:20 +0300

    Complete the how-to-use page

--- client/src/components/pages/how-to-use.js
+++ client/src/components/pages/how-to-use.js
@@ -4,9 +4,11 @@ class HowToUse extends Component {
   render() {
     return (
       <>
-        <div>
+        <div class="howTo">
           <div class="spaceTop"></div>
           <h1>How To Use ?</h1>
+          <hr />
+          <div class="spaceImg"></div>
           <h2>About</h2>
           This dApp allows you to create wallets with contract accounts. You can
           receive and send your money and also you can check your wallet
@@ -20,6 +22,20 @@ class HowToUse extends Component {
             You can't make a money transfer from one contract wallet to another
             contract wallet
           </li>
+          <div class="spaceImg"></div>
+          <h2>Send/Add Money</h2>
+          On the Send Add Money page you can simply add money with add money
+          section like shown in the down :
+          <div>
+          <div class="spaceImg"></div>
+          <img src={require("../images/Add-Money.gif")} width="400" />
+          </div>
+          And you can send some money from your account like shown in the down :
+          <div>
+          <div class="spaceImg"></div>
+          <img src={require("../images/Send-Money.gif")} width="400" />
+          </div>
+          <div class="spaceDown"></div>
         </div>
       </>
     );
